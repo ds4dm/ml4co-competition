a50c0ff8e87b01e278555fa0e11a88f3d303762e
Maxime Gasse <maxime.gasse@gmail.com>
2021-06-19 06:16:21 +0000

    Missing file

---
+++ home/test/init.sh
@@ -0,0 +1,5 @@
+source /opt/mamba/init.bash
+conda env create -n ml4co -f environment.yaml
+conda activate ml4co
+# installation commands for additional dependencies go here
+# e.g., pip install ...
