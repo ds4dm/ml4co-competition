a874baa867c0fcd2ceabb32d239b0df982660ae0
Maxime Gasse <maxime.gasse@gmail.com>
2021-06-22 15:52:12 +0000

    Minor fixes + added an interactive script

--- 99_interactive.sh
+++ 99_interactive.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 
 display_usage() {
-	echo -e "Usage: $0 team task benchmark"
+	echo -e "Usage: $0 [gpu,nogpu]"
 }
 
-# if less than three arguments supplied, display usage
-if [  $# -lt 3 ]
+# if less than one arguments supplied, display usage
+if [  $# -lt 1 ]
 then
 	display_usage
 	exit 1
 fi
 
+case $1 in
+    gpu)
+        export SINGULARITY_NV=1
+        ;;
+    nogpu)
+        export SINGULARITY_NV=0
+        ;;
+    *)
+	display_usage
+	exit 1
+esac
+
 export SINGULARITY_HOME=`realpath home/$1`
 export SINGULARITY_BIND="$(mktemp -d):/tmp,$(mktemp -d):/var/tmp,$(realpath data/instances):$SINGULARITY_HOME/instances:ro"
 export SINGULARITY_CLEANENV=1
 export SINGULARITY_CONTAINALL=1
-export SINGULARITY_NV=1
 export SINGULARITY_NETWORK=none
 
-singularity exec --net base.sif bash run.sh $2 $3
+singularity run base.sif
