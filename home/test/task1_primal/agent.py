333818dddd1cf77621605eb046228ca1d7d79d55
Maxime Gasse <maxime.gasse@gmail.com>
2021-06-23 20:50:07 +0000

    Simplified agent ObservationFunction

--- home/test/task1_primal/agent.py
+++ home/test/task1_primal/agent.py
@@ -5,26 +5,19 @@ import numpy as np
 class ObservationFunction():
 
     def __init__(self, problem):
-        self.milp_bipartite_func = ec.observation.MilpBipartite(normalize=False) 
         self.problem = problem  # to devise problem-specific observations
 
     def before_reset(self, model):
-        self.milp_bipartite_func.before_reset(model)
+        pass
 
     def extract(self, model, done):
         if done:
             return None
 
-        milp = self.milp_bipartite_func.extract(model, done)
-
-        # has_lb = milp.variable_features[:, milp.VariableFeatures.has_lower_bound]
-        # has_ub = milp.variable_features[:, milp.VariableFeatures.has_upper_bound]
-        # lbs = milp.variable_features[:, milp.VariableFeatures.lower_bound]
-        # ubs = milp.variable_features[:, milp.VariableFeatures.upper_bound]
-
         m = model.as_pyscipopt()
         variables = m.getVars()
         inf = m.infinity()
+
         lbs = np.asarray([v.getLbLocal() for v in variables])
         ubs = np.asarray([v.getUbLocal() for v in variables])
         has_lb = np.asarray([lb > -inf for lb in lbs])
